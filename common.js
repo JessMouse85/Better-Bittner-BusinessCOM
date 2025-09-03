2ec3b033d4467a15f332f3ad4a17eba59e8248a7
JessMouse85 <jesse.britten1985@gmail.com>
2025-09-03 02:04:01 -0600

    First push

---
+++ common.js
@@ -0,0 +1,4 @@
+document.getElementById('newsBumperClose').addEventListener('click', function () {
+  document.getElementById('quickHelpBar').style.visibility = 'hidden';
+  document.getElementById('quickHelpBar').style.position = 'absolute';
+});

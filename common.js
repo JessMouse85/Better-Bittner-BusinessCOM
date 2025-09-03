90a9df629e9f52795809fc822f9009d5e21a5c98
JessMouse85 <jesse.britten1985@gmail.com>
2025-09-03 15:19:33 -0600

    Major Update to Prototype

--- common.js
+++ common.js
@@ -2,3 +2,9 @@ document.getElementById('newsBumperClose').addEventListener('click', function ()
   document.getElementById('quickHelpBar').style.visibility = 'hidden';
   document.getElementById('quickHelpBar').style.position = 'absolute';
 });
+
+Array.from(document.getElementsByClassName('serviceListing')).forEach(function (element) {
+  element.addEventListener('click', function () {
+    window.location.href = './ITConsulting.html';
+  });
+});

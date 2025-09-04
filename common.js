d4a100b30f3dfe9dec3e8e19dd3096cf69427d83
JessMouse85 <jesse.britten1985@gmail.com>
2025-09-04 13:41:17 -0600

    Adding relevant files

--- common.js
+++ common.js
@@ -1,10 +1,43 @@
-document.getElementById('newsBumperClose').addEventListener('click', function () {
-  document.getElementById('quickHelpBar').style.visibility = 'hidden';
-  document.getElementById('quickHelpBar').style.position = 'absolute';
-});
+  
+  
+  document.getElementById('newsBumperClose').addEventListener('click', function () {
+    document.getElementById('quickHelpBar').style.visibility = 'hidden';
+    document.getElementById('quickHelpBar').style.position = 'absolute';
+  });
 
-Array.from(document.getElementsByClassName('serviceListing')).forEach(function (element) {
-  element.addEventListener('click', function () {
+  // Technology Consulting Services Click Even Handlers
+  document.getElementById('technologyConsulting').addEventListener('click', function () {
     window.location.href = './ITConsulting.html';
   });
-});
+
+  // Web Development Services Click Event Handler
+  document.getElementById('webDevelopment').addEventListener('click', function () {
+    window.location.href = './IT-Services/webdev.html';
+  });
+
+  document.getElementById('technologyConsulting').addEventListener('click', function () {
+    window.location.href = './ITConsulting.html';
+  });
+
+  document.getElementById('technologyConsulting').addEventListener('click', function () {
+    window.location.href = './ITConsulting.html';
+  });
+
+  document.getElementById('technologyConsulting').addEventListener('click', function () {
+    window.location.href = './ITConsulting.html';
+  });
+
+  document.getElementById('itSecurityConsulting').addEventListener('click', function () {
+    window.location.href = './ITSecurityConsulting.html';
+  });
+
+  document.getElementById('marketingAdCampaigns').addEventListener('click', function () {
+    window.location.href = './MarketingAdCampaigns.html';
+  });
+
+
+  // Array.from(document.getElementsByClassName('serviceListing')).forEach(function (element) {
+  //   element.addEventListener('click', function () {
+  //     window.location.href = './ITConsulting.html';
+  //   });
+  // });

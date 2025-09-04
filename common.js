d5e3297f2e577d85360f7a932ad159faa98a6b0a
JessMouse85 <jesse.britten1985@gmail.com>
2025-09-04 15:34:52 -0600

    Wireframe Concept stage 1

--- common.js
+++ common.js
@@ -5,35 +5,35 @@
     document.getElementById('quickHelpBar').style.position = 'absolute';
   });
 
-  // Technology Consulting Services Click Even Handlers
-  document.getElementById('technologyConsulting').addEventListener('click', function () {
-    window.location.href = './ITConsulting.html';
-  });
+  // // Technology Consulting Services Click Even Handlers
+  // document.getElementById('technologyConsulting').addEventListener('click', function () {
+  //   window.location.href = './ITConsulting.html';
+  // });
 
-  // Web Development Services Click Event Handler
-  document.getElementById('webDevelopment').addEventListener('click', function () {
-    window.location.href = './IT-Services/webdev.html';
-  });
+  // // Web Development Services Click Event Handler
+  // document.getElementById('webDevelopment').addEventListener('click', function () {
+  //   window.location.href = './IT-Services/webdev.html';
+  // });
 
-  document.getElementById('technologyConsulting').addEventListener('click', function () {
-    window.location.href = './ITConsulting.html';
-  });
+  // document.getElementById('technologyConsulting').addEventListener('click', function () {
+  //   window.location.href = './ITConsulting.html';
+  // });
 
-  document.getElementById('technologyConsulting').addEventListener('click', function () {
-    window.location.href = './ITConsulting.html';
-  });
+  // document.getElementById('technologyConsulting').addEventListener('click', function () {
+  //   window.location.href = './ITConsulting.html';
+  // });
 
-  document.getElementById('technologyConsulting').addEventListener('click', function () {
-    window.location.href = './ITConsulting.html';
-  });
+  // document.getElementById('technologyConsulting').addEventListener('click', function () {
+  //   window.location.href = './ITConsulting.html';
+  // });
 
-  document.getElementById('itSecurityConsulting').addEventListener('click', function () {
-    window.location.href = './ITSecurityConsulting.html';
-  });
+  // document.getElementById('itSecurityConsulting').addEventListener('click', function () {
+  //   window.location.href = './ITSecurityConsulting.html';
+  // });
 
-  document.getElementById('marketingAdCampaigns').addEventListener('click', function () {
-    window.location.href = './MarketingAdCampaigns.html';
-  });
+  // document.getElementById('marketingAdCampaigns').addEventListener('click', function () {
+  //   window.location.href = './MarketingAdCampaigns.html';
+  // });
 
 
   // Array.from(document.getElementsByClassName('serviceListing')).forEach(function (element) {
